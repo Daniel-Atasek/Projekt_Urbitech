0f384884b250727aaf76e7b61aa0c4dde6d42ef8
Daniel-Atasek <154004882+Daniel-Atasek@users.noreply.github.com>
2023-12-20 20:18:04 +0100

    Update HomePresenter.php

--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -6,7 +6,6 @@ namespace App\Presenters;
 
 use App\Forms\AuthorForm;
 use App\Forms\BookForm;
-//use App\Model\MainRepository;
 use App\Model\MainService;
 use Nette;
 use Nette\Application\UI\Form;
@@ -30,23 +29,6 @@ final class HomePresenter extends Nette\Application\UI\Presenter
       }
       $this->template->records = $records;
    }
-   //search function, not working yet
-   /*protected function createComponentSearchForm():SearchForm
-   {
-      $form = new SearchForm();
-      $form->onDataSubmitted[] = [$this, 'formSucceededSearch']; 
-      return $form;
-   }
-   public function formSucceededSearch(Form $form, $data){
-      $databaseResult = $this->mainRepository->search($data['option'], $data['searchParameters']);
-      $records = $databaseResult;
-      
-      foreach($records as &$record){
-         $record['description'] = mb_strimwidth($record['description'], 0, 100) . '...';
-      }
-      $this->template->records = $records;
-   }*/
-
    public function createComponentBookForm():BookForm
    {
       $form = new BookForm($this->mainService->findAuthors(), $this->mainService->findBooks());
